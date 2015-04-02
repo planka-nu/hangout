47250505545947eb9a721fff1c703abff63d0468
Jesper Nilsson <jesper@klandestino.se>
2015-04-02 15:06:38 +0200

    Adds tabs.

--- javascripts/client.js
+++ javascripts/client.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
 	var roomDivs = {};
+	var tabList = {};
 
 	var socket = io.connect('//' + document.location.host);
 	socket.on('connect', function() {
@@ -23,15 +24,30 @@ $(document).ready(function () {
 		socket.on('join', function (room) {
 			console.log(room);
 			if (typeof roomDivs[room] === 'undefined') {
-				roomDivs[room] = $('<div class="my2 brdr--light-gray bg--white"><div class="header bg--off-white py1"><div class="grd"><div class="grd-row"><div class="grd-row-col-4-6"><h1></h1></div><div class="grd-row-col-2-6 txt--right py1-5"><a href="#" class="leaveroom btn btn--gray">Leave room</a></div></div></div></div><div class="attendance-list m1">Attendance: <ul class="attendance"></ul></div><div class="m1"><ul class="msgs"></ul></div><div class="footer bg--off-white py1"><div class="grd"><div class="grd-row"><div class="grd-row-col-5-6"><input type="text" class="msg" placeholder="Say something..." /></div><div class="grd-row-col-1-6"><input type="button" class="btn btn--blue msg-send" value="Say!" /></div></div></div></div></div>');
+				tabList[room] = $('<li data-target="'+room+'">'+room+'</li>');
+				roomDivs[room] = $('<div id="'+room+'" class="my2 brdr--light-gray bg--white"><div class="header bg--off-white py1"><div class="grd"><div class="grd-row"><div class="grd-row-col-4-6"><h1></h1></div><div class="grd-row-col-2-6 txt--right py1-5"><a href="#" class="leaveroom btn btn--gray">Leave room</a></div></div></div></div><div class="attendance-list m1">Attendance: <ul class="attendance"></ul></div><div class="m1"><ul class="msgs"></ul></div><div class="footer bg--off-white py1"><div class="grd"><div class="grd-row"><div class="grd-row-col-5-6"><input type="text" class="msg" placeholder="Say something..." /></div><div class="grd-row-col-1-6"><input type="button" class="btn btn--blue msg-send" value="Say!" /></div></div></div></div></div>');
 				$('#rooms').append(roomDivs[room]);
+				$('#tabs ul').append(tabList[room]);
 				$('h1', roomDivs[room]).append(document.createTextNode(room));
 				$('a.leaveroom', roomDivs[room]).click(function () {
 					socket.emit('leave', room);
 				});
 
+				// Activate latest createt room
+				$('#rooms > div').hide();
+				$('#rooms > div').last().show();
+				$('#tabs ul li').removeClass('active');
+				$('#tabs ul li').last().addClass('active');
+
 				var writing = null;
 
+				$('#tabs ul li').click(function (e) {
+					var target = $(this).data('target');
+					$('#tabs ul li').removeClass('active');
+					$(this).addClass('active');
+					$('#rooms > div').hide();
+					$('#'+target).show();
+				});
 				$('input.msg-send', roomDivs[room]).click(function (e) {
 					var msgTarget =  $(this).closest('.footer').find('input.msg');
 					var msg = $(msgTarget).val();
@@ -42,7 +58,7 @@ $(document).ready(function () {
 						clearTimeout(writing);
 						writing = null;
 					}
-					$('input', roomDivs[room]).focus();
+					$(msgTarget, roomDivs[room]).focus();
 				});
 				$('input', roomDivs[room]).keypress(function (e) {
 
